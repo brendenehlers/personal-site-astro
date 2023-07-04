f2de38be3f2c55d1cc158d723f75a76a51d4aad7
Brenden Ehlers <brenden@theehlers.org>
2023-07-03 23:29:10 -0600

    removed emoji and comments

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -12,7 +12,7 @@ type Presentation = {
 
 const presentation: Presentation = {
   mail: "me@brendenehlers.com",
-  title: "Hey, I’m Brenden 👋",
+  title: "Hey, I’m Brenden",
   description:
     "Welcome to my portfolio!  I'm a full-stack software developer based in Denver, Colorado.  I do most of my work in TypeScript and Java.  Currently learning Next.js and Java Spring.  Reach out if there's something I can help you with",
   socials: [
