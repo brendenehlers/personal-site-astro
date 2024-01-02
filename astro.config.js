d7d4a451721204f95e07643091426323929747c4
brendenehlers <brenden@theehlers.org>
2024-01-02 14:35:32 -0700

    optimized website

--- astro.config.js
+++ astro.config.js
@@ -2,16 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { SITE_URL } from "./src/data/config";
+import viteCompression from 'vite-plugin-compression'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: SITE_URL,
-  markdown: {
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "nord",
-      wrap: false,
-    },
-  },
+  vite: {
+    plugins: [viteCompression({ algorithm: 'brotliCompress'})]
+  }
 });
